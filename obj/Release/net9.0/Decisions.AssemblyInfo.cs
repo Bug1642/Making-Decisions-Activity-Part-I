411871db4ccb0671b75fac12d01fa5e3f331c2ac
Bug1642 <hasancan1642@hotmail.com>
2024-12-30 14:49:04 +0300

    First Commit

--- obj/Release/net9.0/Decisions.AssemblyInfo.cs
+++ obj/Release/net9.0/Decisions.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Decisions")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffbd8b95ba06a5837c22768cdd2136048bb49e74")]
 [assembly: System.Reflection.AssemblyProductAttribute("Decisions")]
